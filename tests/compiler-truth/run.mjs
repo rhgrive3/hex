88b1156b601b5b3e4ef65d8d96841d264ede9b9f
rhgrive3 <rhgrive@gmail.com>
2026-08-14 22:12:40 +0900

    Add language compiler-truth matrix entry

--- tests/compiler-truth/run.mjs
+++ tests/compiler-truth/run.mjs
@@ -3,3 +3,4 @@ if (typeof BigInt.prototype.toJSON !== 'function') {
   Object.defineProperty(BigInt.prototype, 'toJSON', { value() { return this.toString(); }, configurable: true });
 }
 await import('./run-core.mjs');
+await import('./language-matrix.mjs');
