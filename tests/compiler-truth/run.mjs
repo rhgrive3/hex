2e818189afef101e3310599b556f4ffa80ba9d27
rhgrive3 <rhgrive@gmail.com>
2026-08-15 00:01:27 +0900

    Run extended compiler truth corpus

--- tests/compiler-truth/run.mjs
+++ tests/compiler-truth/run.mjs
@@ -3,4 +3,5 @@ if (typeof BigInt.prototype.toJSON !== 'function') {
   Object.defineProperty(BigInt.prototype, 'toJSON', { value() { return this.toString(); }, configurable: true });
 }
 await import('./run-core.mjs');
+await import('./extended.mjs');
 await import('./language-matrix.mjs');
