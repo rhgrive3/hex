29c1628fab304c0bd48537e774167a3d47c3c2d9
rhgrive3 <rhgrive@gmail.com>
2026-08-18 16:47:06 +0900

    Phase 5: keep implicit sign extension inside integer family

--- js/targets/architecture/x86_64/effects/index.js
+++ js/targets/architecture/x86_64/effects/index.js
@@ -9,12 +9,15 @@ import { liftX86SimdEffects } from './simd.js';
 import { liftX86SystemEffects } from './system.js';
 import { normalizeX86Instruction, X86_64_MACHINE_EFFECTS_SEMANTIC_VERSION } from './common.js';
 
+function liftX86IntegerFamily(instruction, context) {
+  return liftX86ImplicitSignExtensionEffects(instruction, context) ?? liftX86IntegerEffects(instruction, context);
+}
+
 const FAMILIES = Object.freeze([
   Object.freeze({ id:'control', lift:liftX86ControlEffects }),
   Object.freeze({ id:'memory', lift:liftX86MemoryEffects }),
   Object.freeze({ id:'lea', lift:liftX86LeaEffects }),
-  Object.freeze({ id:'implicit-sign-extension', lift:liftX86ImplicitSignExtensionEffects }),
-  Object.freeze({ id:'integer', lift:liftX86IntegerEffects }),
+  Object.freeze({ id:'integer', lift:liftX86IntegerFamily }),
   Object.freeze({ id:'string', lift:liftX86StringEffects }),
   Object.freeze({ id:'atomic', lift:liftX86AtomicEffects }),
   Object.freeze({ id:'fp', lift:liftX86FloatingPointEffects }),
